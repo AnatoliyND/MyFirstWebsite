c79617987136dc5cb2be9f4b6075caded0522f0a
Anatoliy Dubovik <huykilik@gmail.com>
2022-05-03 16:28:28 +0300

    main satings v_0.0.004

--- main.go
+++ main.go
@@ -2,46 +2,26 @@ package main
 
 import (
 	"fmt"
-	"html/template" //пакет для вывода полноценных html страниц
+	"html/template"
 	"net/http"
 )
 
-type User struct {
-	Name                  string
-	Age                   uint16
-	Money                 int16
-	Avg_grades, Happiness float64
-	Hobbies               []string
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "index", nil) //используем ExecuteTemplate(), т.к. внутри шаблонов будем создавать динамическое подключение
 }
 
-func (u User) getAllInfo() string {
-	return fmt.Sprintf("User Name is: %s. He is %d Age and "+
-		"he has Money equal: %d", u.Name, u.Age, u.Money)
-}
-
-func (u *User) setNewName(newName string) {
-	u.Name = newName
-}
-
-func home_page(w http.ResponseWriter, r *http.Request) {
-	ben := User{"Ben", 24, -105, 4.5, 0.9, []string{"Football", "Skate", "Dance"}}
-	/* 	fmt.Fprintf(w, `<h1>Main Text</h1>
-	   	<b>Main Taxt</b1>`) //формат для вывода HTML страниц(приметивный)
-	*/
-	tmpl, _ := template.ParseFiles("templates/home_page.html") //создаем переменную, которая будет хранить шаблон для html страницы
-	tmpl.Execute(w, ben)                                       //отображение шаблона на странице
-}
-
-func contacts_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Contacts page!")
-}
-
-func handleRequest() {
-	http.HandleFunc("/", home_page)
-	http.HandleFunc("/contacts/", contacts_page)
+func handleFunc() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
-	handleRequest()
+	handleFunc()
 }
